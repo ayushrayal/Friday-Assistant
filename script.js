11cf9417d39e15a1f68878bdf5261473a1754089
ayushrayal <ayushrayal19@gmail.com>
2025-11-05 22:52:38 +0530

    Update index.html and script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-let btn = document.getElementById("#btn");
+let btn = document.getElementById("btn");
+let content = document.getElementById("content");
 
-btn.addEventListener("click", function() {
-    alert("Button was clicked!");
-});
+function speak(text){
+    let text_speak = new SpeechSynthesisUtterance(text);
+    text_speak.lang = "hi-GB";
+    text_speak.volume = 1; // 0 to 1
+    text_speak.rate = 1; // 0.1 to 10
+    text_speak.pitch = 2; // 0 to 2
+    window.speechSynthesis.speak(text_speak);
+}
+btn.addEventListener("click", ()=>{
+    let text = content.value;
+    speak(text);
+})
